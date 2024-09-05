46ec25b3f5142629056331afdc20368857df084a
EugenieKim <eugenie.kim@outlook.com>
2024-09-05 14:12:53 -0700

    Added some module uploads and a connection to mongodb; Connection does not currently work on my device and needs to be tested on a different computer Created an empty home page file for testing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("./utils.js");
-require('dotenv').config();
+const dotenv = require('dotenv').config()
 const express = require('express');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
@@ -9,7 +9,6 @@ const bcrypt = require('bcrypt');
 
 const cron = require('node-cron');
 
-const {Storage} = require('@google-cloud/storage');
 const {Readable} = require('stream');
 const crypto = require('crypto');
 const { pipeline } = require('stream/promises');
@@ -19,8 +18,53 @@ const path = require('path');
 
 const multer = require("multer");
 const stream = require("stream");
-const cloudinary = require('cloudinary').v2;
+// const cloudinary = require('cloudinary').v2;
 
-const Swal = require('sweetalert2');
+// const Swal = require('sweetalert2');
 const Queue = require('bull');
 const saltRounds = 10;
+
+const app = express();
+const port = process.env.PORT || 3000;
+const Joi = require('joi');
+const ejs = require('ejs');
+
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_appdb = process.env.MONGODB_APPDATABASE;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+
+//Creating a MongoClient and using it to connect to a specified database
+const MongoClient = require("mongodb").MongoClient;
+const appdb = new MongoClient(`mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/${mongodb_appdb}?retryWrites=true`);
+
+// ----- Collections -----
+const trainersdb = appdb.db(mongodb_appdb).collection('trainers');
+const classesdb = appdb.db(mongodb_appdb).collection('classes');
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// This is for storing active sessions
+var mongoStore = MongoStore.create({
+	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
+	crypto: {
+		secret: mongodb_session_secret
+	}
+});
+
+app.use(session({
+	secret: node_session_secret,
+	store: mongoStore, //default is memory store 
+	saveUninitialized: false,
+	resave: true
+}));
+
+app.get('/', async (req, res) => {
+    res.render('home');
+});
